c0472e26c3d682c0cb59907e0b90c9b008136e3c
Zhivko Shopov <zhishopov@gmail.com>
2025-08-26 17:09:00 +0100

    Create the board

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,40 @@
-function App() {}
+function App() {
+  return (
+    <>
+      <div className="board">
+        <div className="row">
+          <button>AC</button>
+          <button>+/-</button>
+          <button>%</button>
+          <button>/</button>
+        </div>
+        <div className="row">
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>X</button>
+        </div>
+        <div className="row">
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>-</button>
+        </div>
+        <div className="row">
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>+</button>
+        </div>
+        <div className="row">
+          <button>ON</button>
+          <button>0</button>
+          <button>.</button>
+          <button>=</button>
+        </div>
+      </div>
+    </>
+  );
+}
 
 export default App;
